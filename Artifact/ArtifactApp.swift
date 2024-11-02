6c42d78a3aff2ecaa3e0c07231e0ce439b88f167
redenza <redenza@twitch.tv>
2024-11-02 10:33:17 -0400

    Initial Commit

---
+++ Artifact/ArtifactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtifactApp.swift
+//  Artifact
+//
+//  Created by Reese, Wolf on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ArtifactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Artifact/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Artifact
+//
+//  Created by Reese, Wolf on 11/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
